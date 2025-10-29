176a311541f9f8169aeb42975cd46fed95f372e6
Neil Darach <neil.darach@gmail.com>
2025-10-29 20:26:22 +0100

    Maybe added various systemd services to match a strongStateDir enablement

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
               default = name;
               description = "Base name for all the config elements";
             };
+            dirName = lib.mkOption {
+              type = lib.types.str;
+              default = name;
+              description = "Name of the dataset and mount point";
+            };
 
           };
         });
@@ -37,17 +42,65 @@
         wrappedMkStrongStateDir = pkgs.symlinkJoin {
           name = "mkStrongStateDir";
           paths = [ mkStrongStateDir ]
-            ++ (with pkgs; [ gzip openssh gnugrep util-linux ]);
+            ++ (with pkgs; [ gzip openssh gnugrep util-linux zfs ]);
           buildInputs = [ pkgs.makeWrapper ];
           postBuild =
             "wrapProgram $out/bin/mkStrongStateDir --prefix PATH : $out/bin";
         };
-      in {
-        environment.etc.strongStateDir = {
-          text = "${wrappedMkStrongStateDir}/bin/mkStrongStateDir "
-            + (lib.concatStringsSep " "
-              (map (ea: lib.escapeShellArg ea.serviceName) enabledServices));
+        zfsBackupSrc = builtins.readFile ./zfs-backup;
+        zfsBackup =
+          (pkgs.writeScriptBin "zfs-backup" zfsBackupSrc).overrideAttrs (p: {
+            buildCommand = ''
+              ${p.buildCommand}
+              patchShebangs $out
+            '';
+          });
+        wrappedZfsBackup = pkgs.symlinkJoin {
+          name = "zfs-backup";
+          paths = [ zfsBackup ] ++ (with pkgs; [ gzip openssh coreutils zfs ]);
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild =
+            "wrapProgram $out/bin/zfs-backup --prefix PATH : $out/bin";
         };
+      in {
+        systemd.services = {
+          "zfs-backup@" = lib.mkIf (lib.len enabledServices > 0) {
+            description = "Backup a strong state dir";
+            serviceConfig = {
+              Type = "oneshot";
+              User = "root";
+              ExecStart = "${wrappedZfsBackup}/bin/zfs-backup %i";
+            };
+          };
+          "strongStateDir@" = lib.mkIf (lib.len enabledServices > 0) {
+            description = "Set up the state dir";
+            serviceConfig = {
+              Type = "oneshot";
+              User = "root";
+              ExecStart = "${wrappedMkStrongStateDir}/bin/mkStrongStateDir %i";
+            };
+          };
+        } // lib.mapAttrs (ea:
+          let
+            user = config.systemd.services.${ea.serviceName}.serviceConfig.User;
+            group =
+              config.systemd.services.${ea.serviceName}.serviceConfig.Group;
+          in {
+            "${ea.serviceName}".serviceConfig.wants = [
+              "strongStateDir@${ea.dirName}:${user}:${group}:${ea.serviceName}.service"
+            ];
+          }) enabledServices;
+
+        systemd.timers = lib.mapAttrs (ea: {
+          "strongStateDir-backup-${ea.serviceName}" = {
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+              OnCalendar = "Sun 20:03:00";
+              RandomizedDelaySec = "1200";
+              Unit = "zfs-backup@${ea.dirName}:${ea.serviceName}";
+            };
+          };
+        }) enabledServices;
       };
     };
   };
